d88cc5a28cf3f5fffeb26c04fd668ab85359861a
Alfredo Cabrera <alfredocabrera.tech@gmail.com>
2022-10-01 20:57:55 -0400

    new try

---
+++ config/allowedOrigins.js
@@ -0,0 +1,6 @@
+const allowedOrigins = [
+  'https://nerd-songwriter-api-pnnd.onrender.com',
+  'https://nerd-songwriter-backend-7umm5sxs9-blackvikingcodes.vercel.app'
+]
+
+module.exports = allowedOrigins
--- server.js
+++ server.js
@@ -6,10 +6,11 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const logger = require('morgan')
 
-
+const rootRoute = require('./routes/root')
 const songRoutes = require('./routes/songs')
 const userRoutes = require('./routes/user')
 const connectDB = require('./config/database')
+const allowedOrigins = require('./config/allowedOrigins')
 
 // express app
 const app = express()
@@ -17,8 +18,17 @@ const app = express()
 
 // CORS
 app.use(cors({
-  origin: "*",
-}))
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        callback(null, true)
+    } else {
+        callback(new Error('Not allowed by CORS'))
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200
+  }
+))
 
 
 // middleware
@@ -36,8 +46,20 @@ app.use(logger('dev'))
 */
 
 // routes
+app.use('/', rootRoute)
 app.use('/api/songs', songRoutes)
 app.use('/api/user', userRoutes)
+
+app.all('*', (req, res) => {
+  res.status(404)
+  if (req.accepts('html')) {
+      res.sendFile(path.join(__dirname, 'views', '404.html'))
+  } else if (req.accepts('json')) {
+      res.json({ message: '404 Not Found' })
+  } else {
+      res.type('txt').send('404 Not Found')
+  }
+})
 
 // connect to db
 connectDB()
