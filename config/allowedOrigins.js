fb44b9687259df432c3de508e47fb98e34b9f863
Alfredo Cabrera <alfredocabrera.tech@gmail.com>
2022-10-01 22:37:00 -0400

    more routes added to cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,17 @@
 const allowedOrigins = [
-  'https://nerd-songwriter-x9np.onrender.com'
+  'https://nerd-songwriter-x9np.onrender.com/',
+  'https://nerd-songwriter-x9np.onrender.com/api/',
+  'https://nerd-songwriter-x9np.onrender.com/api/user/',
+  'https://nerd-songwriter-x9np.onrender.com/api/user/login',
+  'https://nerd-songwriter-x9np.onrender.com/api/user/signup',
+  'https://nerd-songwriter-x9np.onrender.com/api/user/logout',
+  'https://nerd-songwriter-x9np.onrender.com/api/songs/',
+  'https://nerd-songwriter.vercel.app/',
+  'https://nerd-songwriter.vercel.app/api/',
+  'https://nerd-songwriter.vercel.app/api/user/',
+  'https://nerd-songwriter.vercel.app/api/user/login',
+  'https://nerd-songwriter.vercel.app/api/user/signup',
+  'https://nerd-songwriter.vercel.app/api/songs/'
 ]
 
 module.exports = allowedOrigins
